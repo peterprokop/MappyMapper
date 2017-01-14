07d26ba9a2a24b29e2a64f6f1fb439d4f3d11e27
Peter Prokop <pitrowech@gmail.com>
2017-01-14 14:36:18 +0100

    Add Xcode project

---
+++ MappyMapper/MappyMapper.h
@@ -0,0 +1,19 @@
+//
+//  MappyMapper.h
+//  MappyMapper
+//
+//  Created by Peter Prokop on 14/01/17.
+//  Copyright © 2017 Prokop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MappyMapper.
+FOUNDATION_EXPORT double MappyMapperVersionNumber;
+
+//! Project version string for MappyMapper.
+FOUNDATION_EXPORT const unsigned char MappyMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MappyMapper/PublicHeader.h>
+
+
